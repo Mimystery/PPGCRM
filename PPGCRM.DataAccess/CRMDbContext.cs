2732c8817e8ee3e818bce2951fedaf3a43cf924a
Mimystery <vlad1503kudyrko@gmail.com>
2025-08-01 23:41:05 +0200

    Clean Architecture Template Added Back

---
+++ PPGCRM.DataAccess/CRMDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace PPGCRM.DataAccess
+{
+    public class CRMDbContext : DbContext
+    {
+        public CRMDbContext(DbContextOptions<CRMDbContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Configure your entities here
+            // Example: modelBuilder.Entity<YourEntity>().ToTable("YourTableName");
+        }
+        // Define DbSets for your entities
+        // public DbSet<YourEntity> YourEntities { get; set; }
+    }
+}
