6fa14a5d2ec7b02db8279ff5e184043df6061a5f
RaysonKa <bchumak66@gmail.com>
2025-08-01 15:10:48 +0300

    enhance login component with new layout and form validation

--- FrontEnd/ppg-crm/src/app/core/auth/pages/login/login.ts
+++ FrontEnd/ppg-crm/src/app/core/auth/pages/login/login.ts
@@ -1,29 +1,36 @@
-import { Component } from '@angular/core';
-import {FormBuilder, FormGroup, FormsModule, Validators} from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { NonNullableFormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
 import { NzFormModule } from 'ng-zorro-antd/form';
-import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
+import { NzInputModule } from 'ng-zorro-antd/input';
 
 
 @Component({
   selector: 'app-login',
-  imports:[NzFormModule, NzCardModule, FormsModule],
+  imports: [ReactiveFormsModule, NzButtonModule, NzCheckboxModule, NzFormModule, NzInputModule],
   templateUrl: './login.html',
   styleUrl: './login.less',
 })
 
 export class LoginComponent {
-  validateForm: FormGroup;
-  submitted = false;
-  loading = false;
-
-  constructor(private fb: FormBuilder) {
-    this.validateForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required]]
-    });
-  }
+  private fb = inject(NonNullableFormBuilder);
+  validateForm = this.fb.group({
+    username: this.fb.control('', [Validators.required]),
+    password: this.fb.control('', [Validators.required]),
+    remember: this.fb.control(true)
+  });
 
   submitForm(): void {
-    this.submitted = true;
+    if (this.validateForm.valid) {
+      console.log('submit', this.validateForm.value);
+    } else {
+      Object.values(this.validateForm.controls).forEach(control => {
+        if (control.invalid) {
+          control.markAsDirty();
+          control.updateValueAndValidity({ onlySelf: true });
+        }
+      });
     }
+}
 }
