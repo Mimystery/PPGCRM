e58d400843eb3722999cc574513aec381537cbd0
RaysonKa <bchumak66@gmail.com>
2025-08-12 11:04:44 +0300

    Refactor project card components for improved structure and styling; enhance metadata display and action handling

---
+++ FrontEnd/ppg-crm/src/app/features/project-details/process-modal/process-modal.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-process-modal',
+  imports: [],
+  templateUrl: './process-modal.html',
+  styleUrl: './process-modal.less'
+})
+export class ProcessModalComponent {
+
+}
