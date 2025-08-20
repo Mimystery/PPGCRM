4864971a672e1271caed743667eceedd356b89c0
Mimystery <vlad1503kudyrko@gmail.com>
2025-08-20 19:48:56 +0200

    Try 2 to fix conflicts

--- FrontEnd/ppg-crm/src/app/features/kanban/kanban-card/kanban-card.ts
+++ FrontEnd/ppg-crm/src/app/features/kanban/kanban-card/kanban-card.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { NzButtonModule } from "ng-zorro-antd/button";
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { ProcessCardComponent } from "../process-card/process-card";
