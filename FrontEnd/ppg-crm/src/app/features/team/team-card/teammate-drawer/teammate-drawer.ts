1f6736237843113984d46c70ae15d2105309b0e3
RaysonKa <bchumak66@gmail.com>
2025-08-23 03:46:01 +0300

    front: teamdrawer done fully

--- FrontEnd/ppg-crm/src/app/features/team/team-card/teammate-drawer/teammate-drawer.ts
+++ FrontEnd/ppg-crm/src/app/features/team/team-card/teammate-drawer/teammate-drawer.ts
@@ -3,10 +3,18 @@ import {NzDrawerModule} from 'ng-zorro-antd/drawer';
 import {NzCardModule} from 'ng-zorro-antd/card';
 import {NzTagModule} from 'ng-zorro-antd/tag';
 import {NzProgressModule} from 'ng-zorro-antd/progress';
+import { NzDividerModule } from 'ng-zorro-antd/divider';
+import {NzFlexModule} from 'ng-zorro-antd/flex';
+import {NzSelectModule} from 'ng-zorro-antd/select';
+import {NzTableModule} from 'ng-zorro-antd/table';
+import {NzImageModule} from 'ng-zorro-antd/image';
+import {NzIconModule} from 'ng-zorro-antd/icon';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-teammate-drawer',
-  imports: [NzDrawerModule, NzCardModule, NzTagModule, NzProgressModule],
+  imports: [NzDrawerModule, NzCardModule, NzTagModule, NzProgressModule, NzDividerModule, NzFlexModule, NzSelectModule, NzIconModule, CommonModule,
+    NzTableModule, NzImageModule],
   templateUrl: './teammate-drawer.html',
   styleUrl: './teammate-drawer.less'
 })
@@ -14,9 +22,45 @@ export class TeammateDrawerComponent {
   isVisible = input.required();
   @Output() closed = new EventEmitter<void>();
 
+  isEditingName = false;
+  isEditingUsername = false;
+  isEditingRole = false;
+  isEditingRate = false;
+  isEditingPhone = false;
+  isEditingEmail = false;
   handleClose() {
     this.closed.emit();
   }
+  startEditingTeammateField(field: string) {
+    if (field === 'name') {
+      this.isEditingName = true;
+    }
+    if (field === 'username') {
+      this.isEditingUsername = true;
+    }
+    if (field === 'role') {
+      this.isEditingRole = true;
+    }
+    if (field === 'rate') {
+      this.isEditingRate = true;
 
+    }
+    if (field === 'phone') {
+      this.isEditingPhone = true;
+
+    }
+    if (field === 'email') {
+      this.isEditingEmail = true;
+    }
+  }
+
+  finishEditingTeammateField(field: string) {
+    if (field === 'name') this.isEditingName = false;
+    if (field === 'username') this.isEditingUsername = false;
+    if (field === 'role') this.isEditingRole = false;
+    if (field === 'rate') this.isEditingRate = false;
+    if (field === 'phone') this.isEditingPhone = false;
+    if (field === 'email') this.isEditingEmail = false;
+  }
   protected readonly window = window;
 }
